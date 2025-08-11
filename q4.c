7ad9d9c3a899c27a165f57ddae17d4700c451fa7
mrtj32 <23103302@mail.jiit.ac.in>
2025-08-11 23:40:51 +0530

    q2.c

--- q4.c
+++ q4.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    int key, i;
-    char data[100];
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
 
-    printf("Enter the plain text: ");
-    fgets(data, sizeof(data), stdin);
+    int matrix[rows][cols], transpose[cols][rows];
 
-    printf("Enter the key value: ");
-    scanf("%d", &key);
-
-    for (i = 0; i < strlen(data); i++) {
-        if (data[i] >= 'A' && data[i] <= 'Z') {
-            data[i] = ((data[i] - 'A' + key) % 26) + 'A';
-        } else if (data[i] >= 'a' && data[i] <= 'z') {
-            data[i] = ((data[i] - 'a' + key) % 26) + 'a';
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
 
-    printf("Your cipher text is: %s\n", data);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
